2213134aba6627b0aa6232b0f05035d0bf5ba048
Khaled Hesham <khaledhesham007@gmail.com>
2024-10-25 23:12:48 +0300

    startup

---
+++ API/Data/ETContext.cs
@@ -0,0 +1,9 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class ETContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<User> Users { get; set; }
+}
