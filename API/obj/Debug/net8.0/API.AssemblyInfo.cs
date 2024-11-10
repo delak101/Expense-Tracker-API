6c42bbf0842e4705cd418d19e89ecb337d6f69b1
Khaled Hesham <khaledhesham007@gmail.com>
2024-11-10 22:31:53 +0200

    GitIgnore

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc508aab7e4ff3c463d6322aa3b2e55a51f38a96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2efe960ddd65bdcf009031963a43609a0c7b4228")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
